5801a6ccd97754296d95f07d014e29afed66e54d
ertugrulkulaber <92637137+ertugrulkulaber@users.noreply.github.com>
2021-10-26 18:43:08 +0300

    Initial Commit

---
+++ EkranlarlaCalisma/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EkranlarlaCalisma
+//
+//  Created by Ertuğrul Kulaber on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
